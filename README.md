d1c70e26d0dec3e34cf3c88d28169e00ff9b4dd7
Santinosabatini <sc.sabatini@alumno.etec.um.edu.ar>
2024-06-05 11:48:13 -0300

    Clase del 5 de junio

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hola-mundo-express
+# Santino Sabatini
 ## Run Locally  
 Clone the project  
 
--- index.js
+++ index.js
@@ -9,3 +9,12 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
+
+app.get('/saludo', (req, res) => {
+  res.send('Hola muy buenas');
+});
+
+app.get('/saludo/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send('Hola ' + nombre);
+});
